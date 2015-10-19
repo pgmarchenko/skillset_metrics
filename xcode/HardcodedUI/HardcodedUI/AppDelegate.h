577a5b0869f1f19f3aa879d012984d86d9f151e7
Pavel Marchenko <pgmarchenko@gmail.com>
2015-10-19 22:30:48 +0300

    Init commit

---
+++ xcode/HardcodedUI/HardcodedUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HardcodedUI
+//
+//  Created by Pavel Marchenko on 10/19/15.
+//  Copyright © 2015 Pavel Marchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
